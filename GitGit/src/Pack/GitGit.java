f369133867cc47f377010a9e8f3a21fe06bd234f
kangdongha97 <kangdongha97>
2021-09-07 17:29:10 +0900

    init

---
+++ GitGit/src/Pack/GitGit.java
@@ -0,0 +1,10 @@
+package Pack;
+
+public class GitGit {
+
+	public static void main(String[] args) {
+		System.out.println("æ»≥Á«œººø‰");
+		System.out.println("æ»¥®");
+	}
+
+}
---
+++ GitGit/src/Pack/Gitfirst.java
@@ -0,0 +1,10 @@
+package Pack;
+
+
+public class Gitfirst {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+
+}
